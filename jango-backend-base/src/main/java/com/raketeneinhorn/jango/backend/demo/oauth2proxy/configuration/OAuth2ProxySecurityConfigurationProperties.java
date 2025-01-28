9a05ef399294f7028ed8450ae99f15577e7bd454
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2025-01-28 21:50:48 +0100

    chore: sonar

--- jango-backend-base/src/main/java/com/raketeneinhorn/jango/backend/demo/oauth2proxy/configuration/OAuth2ProxySecurityConfigurationProperties.java
+++ jango-backend-base/src/main/java/com/raketeneinhorn/jango/backend/demo/oauth2proxy/configuration/OAuth2ProxySecurityConfigurationProperties.java
@@ -9,8 +9,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("jango.backend.demo.oauth2proxy")
 public class OAuth2ProxySecurityConfigurationProperties {
 
-    public static final String AUTH_PATH_PREFIX_DEFAULT = "/demo/oauth2-proxy/auth";
-    public static final String NOAUTH_PATH_PREFIX_DEFAULT = "/demo/oauth2-proxy/noauth";
+    public static final String PATH_PREFIX = "/demo/oauth2-proxy";
+    public static final String AUTH_PATH_PREFIX_DEFAULT = PATH_PREFIX + "/auth";
+    public static final String NOAUTH_PATH_PREFIX_DEFAULT = PATH_PREFIX + "/noauth";
 
     private String authPathPrefix = AUTH_PATH_PREFIX_DEFAULT;
     private String noauthPathPrefix = NOAUTH_PATH_PREFIX_DEFAULT;
